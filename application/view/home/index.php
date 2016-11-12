139ff602754fdef4e5d4469efec2f1e3248e78a4
jonzuck <jonzuck@gmail.com>
2016-11-12 18:56:17 -0500

    Reverted changes due to merge conflict.

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,4 +1,14 @@
-<div class="container">
-    <h2>You are in the View: application/view/home/index.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be the homepage.</p>
+<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4 login">
+    <h1>Please log in</h1>
+    <form action="index.php" method="get">
+        <div class="form-group">
+            <label for="username">Your Username</label>
+            <input type="username" class="form-control" id="username" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-default pull-right">Submit</button>
+    </form>
 </div>
