b0b622a8740e52bafbbda1dcfa9ca381204870f3
dmileyko <dmileyko@gmail.com>
2016-11-12 16:37:06 -0500

    .

--- application/controller/login.php
+++ application/controller/login.php
@@ -9,12 +9,25 @@
  */
 class Login extends Controller
 {
+    var $loginError;
     /**
      * PAGE: index
      * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
      */
     public function index()
     {
+        if (isset($_POST['submit'])){              
+            $employeeModel = $this->loadModel('employee');
+            $user = $employeeModel->get_by_username($_POST['username']);
+            if ($user && $user->password != $_POST['password']) {
+                $this->loginError = "Incorrect Username or Password.";
+            } else if ($user && $user->password == $_POST['password']) { 
+                $_SESSION['current_user'] = $user;
+                header('location: ' . APP . 'home');
+            } else 
+                $this->loginError = "Incorrect Username or Password.";
+        }
+
         // load views
         require APP . 'view/_templates/header.php';
         require APP . 'view/login/index.php';
@@ -25,11 +38,12 @@ class Login extends Controller
      * This method handles what happens when you move to http://yourproject/home/exampleone
      * The camelCase writing is just for better readability. The method name is case-insensitive.
      */
-    public function Login()
+    
+    public function test()
     {
-        // load views
         require APP . 'view/_templates/header.php';
-        require APP . 'view/home/example_one.php';
+        require APP . 'view/login/test.php';
         require APP . 'view/_templates/footer.php';
+        // load views        
     }
 }
