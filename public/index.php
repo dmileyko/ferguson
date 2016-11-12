cf3b1328a67a93957cbca535c244aa838c0314b1
dmileyko <dmileyko@gmail.com>
2016-11-12 00:05:20 -0500

    Added index.php

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// TODO get rid of this and work with namespaces + composer's autoloader
+// set a constant that holds the project's folder path, like "/var/www/".
+// DIRECTORY_SEPARATOR adds a slash to the end of the path
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+// set a constant that holds the project's "application" folder, like "/var/www/application".
+define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
+// This is the (totally optional) auto-loader for Composer-dependencies (to load tools into your project).
+// If you have no idea what this means: Don't worry, you don't need it, simply leave it like it is.
+if (file_exists(ROOT . 'vendor/autoload.php')) {
+    require ROOT . 'vendor/autoload.php';
+}
+// load application config (error reporting etc.)
+require APP . 'config/config.php';
+//require APP . 'libs/helper.php';
+// load application class
+require APP . 'core/application.php';
+require APP . 'core/controller.php';
+// start the application
+$app = new Application();
