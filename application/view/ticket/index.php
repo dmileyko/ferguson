f3de2269f91c629767b16719f472c23f248f0f8f
jonzuck <jonzuck@gmail.com>
2016-11-12 20:19:44 -0500

    corrected image paths, names on forms

--- application/view/ticket/index.php
+++ application/view/ticket/index.php
@@ -1,2 +1,21 @@
-<?php
+<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4 new-ticket fade-in">
+    <h1>Please log in</h1>
+    <form action="index.php" method="get">
+        <div class="form-group">
+            <label for="custname">Customer Name</label>
+            <label for="custname">Customer Name</label>
+            <label for="custname">Customer Name</label>
+            <input type="username" class="form-control" id="username" placeholder="Customer Name">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea type="description" class="form-control" id="description" placeholder="Description">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea type="description" class="form-control" id="description" placeholder="Description">
+        </div>        
+        <button type="submit" class="btn btn-default pull-right">Submit</button>
+    </form>
+</div>
  
