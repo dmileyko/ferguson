09504961cf78c5d81c6d3447b8444ef49bac27c1
dmileyko <dmileyko@gmail.com>
2016-11-12 19:42:34 -0500

    Added ticket controller and view

---
+++ application/controller/ticket.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class Home
+ *
+ * Please note:
+ * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
+ * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
+ *
+ */
+class ticket extends Controller
+{
+    var $loginError;
+    /**
+     * PAGE: index
+     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
+     */
+    public function index()
+    {           
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/ticket/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+    /**
+     * PAGE: exampleone
+     * This method handles what happens when you move to http://yourproject/home/exampleone
+     * The camelCase writing is just for better readability. The method name is case-insensitive.
+     */
+    
+}
